f7781cadfa5b4ed52f707f02e98ca07e660130a4
kormilov_vk <vadim.kormilov@gmail.com>
2019-01-07 23:57:45 +0600

    add config

--- webpack.config.js
+++ webpack.config.js
@@ -4,63 +4,69 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-    entry: ['babel-polyfill', './src/index.js'],
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
-    },
-    devServer: {
-        contentBase: './dist',
-        historyApiFallback: true,
-        hot: true
-    },
-    plugins: [
-        new webpack.ProgressPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new HtmlWebpackPlugin({
-            inject: false,
-            template: require('html-webpack-template'),
-            headHtmlSnippet: '',
-            appMountId: 'app'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                            importLoaders: 1
+module.exports = (env, options) => {
+    const { mode } = options
+    const isProd = mode === 'production'
+
+    const optimization = isProd ? { minimizer: [new UglifyJsPlugin()] } : {}
+    const config = '{"apiRoot": "http://kormilov_vk.hldns.ru/api/"}'
+
+    return {
+        entry: ['babel-polyfill', './src/index.js'],
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: '/'
+        },
+        devServer: {
+            contentBase: './dist',
+            historyApiFallback: true,
+            hot: true
+        },
+        plugins: [
+            new webpack.ProgressPlugin(),
+            new webpack.HotModuleReplacementPlugin(),
+            new HtmlWebpackPlugin({
+                inject: false,
+                template: require('html-webpack-template'),
+                headHtmlSnippet: isProd ? `<script>window.config = ${config}</script>` : '',
+                appMountId: 'app'
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader']
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: 'style-loader'
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true,
+                                importLoaders: 1
+                            }
+                        },
+                        {
+                            loader: 'sass-loader'
                         }
-                    },
-                    {
-                        loader: 'sass-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    optimization: {
-        minimizer: [new UglifyJsPlugin()],
-    },
+                    ]
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['*', '.js', '.jsx']
+        },
+        optimization,
+    }
 };
